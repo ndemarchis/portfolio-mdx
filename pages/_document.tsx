3ab280dbe38575cf7d734024ffa58aa4149d95c3
Nicholas DeMarchis <nicholas.demarchis@unitedmasters.com>
2022-09-05 22:05:33 -0400

    componentized

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { SocialIcon } from "react-social-icons";
+import Header from "../src/components/Header";
 
 const MyDocument = () => {
-  const socialItems = [
-    "http://linkedin.com/in/nickdemarchis",
-    "http://github.com/ndemarchis",
-    "mailto:nick@nickdemarchis.com",
-  ].map((item, index) => (
-    <SocialIcon
-      key={index}
-      url={item}
-      bgColor="#00000000"
-      fgColor="#ffffff"
-      className="h-10"
-    />
-  ));
-
   return (
     <Html lang="en">
       <Head>
-        <div className="flex justify-evenly flex-row items-center w-full p-5">
-          <div className="">
-            <h1 className="">nick&nbsp;deMarchis</h1>
-          </div>
-          <div className="">{socialItems}</div>
-        </div>
+        <Header className="m-auto md:w-10/12 max-w-5xl" />
+        <link
+          rel="preload"
+          href="/fonts/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React, { HTMLProps } from "react";
+import { SocialIcon } from "react-social-icons";
+
+const Header = (props: { className: string }) => {
+  const socialItems = [
+    "http://linkedin.com/in/nickdemarchis",
+    "http://github.com/ndemarchis",
+    "mailto:nick@nickdemarchis.com",
+  ].map((item, index) => (
+    <SocialIcon
+      key={index}
+      url={item}
+      bgColor="#00000000"
+      fgColor="#ffffff"
+      className="h-10"
+    />
+  ));
+
+  return (
+    <div
+      className={`${props.className} flex justify-evenly flex-row items-center w-full p-5`}
+    >
+      <h1 className="w-full">nick&nbsp;deMarchis</h1>
+      <div className="flex flex-row-reverse">{socialItems}</div>
+    </div>
+  );
+};
+
+export default Header;

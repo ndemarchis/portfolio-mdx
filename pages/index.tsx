6a0b269d6311f736bb27749098e953055f0b6300
Nicholas DeMarchis <nicholas.demarchis@unitedmasters.com>
2022-12-05 23:07:40 -0500

    title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 import SocialIcons from "../src/components/SocialIcons";
 
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full gap-2">
+      <Head>
+        <title>nick deMarchis</title>
+      </Head>
       <h1 className="transition-all font-black hover:font-extralight hover:scale-110">
         nick deMarchis
       </h1>

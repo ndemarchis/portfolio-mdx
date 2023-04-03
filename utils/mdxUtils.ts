d51b0f29b2692584143a75b5868c58ba546dc9d8
Nick DeMarchis <ned004@bucknell.edu>
2023-04-03 12:32:23 -0400

    miniscule refactor

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const ARTICLES_PATH = path.join(process.cwd(), 'articles')
+export const ARTICLES_PATH = path.join(process.cwd(), 'public/articles')
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const articleFilePaths = fs

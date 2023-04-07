ec76dcf6e09e930abeae6732646543f7cb271baa
Nick DeMarchis <ndemarchis@seatgeek.com>
2023-04-06 20:12:45 -0400

    my-auto

--- src/components/articles/NormalArticle.tsx
+++ src/components/articles/NormalArticle.tsx
@@ -1,10 +1,13 @@
 import ArticleMeta from "./ArticleMeta";
 
-const NormalArticle = (props: { children: any; metaProps?: ArticleMetaProps }) => {
+const NormalArticle = (props: {
+  children: any;
+  metaProps?: ArticleMetaProps;
+}) => {
   return (
     <>
       {props.metaProps && <ArticleMeta {...props.metaProps} />}
-      <div className="flex-col flex px-5 py-10 gap-5 text-lg [&>*]:m-auto [&>*]:md:w-1/2 [&>*]:max-w-5xl ">
+      <div className="flex-col flex px-5 py-10 gap-5 text-lg my-auto [&>*]:m-auto [&>*]:md:w-1/2 [&>*]:max-w-5xl ">
         {props.children}
       </div>
     </>

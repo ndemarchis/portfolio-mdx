c9dbc92fe307639c7a427170331e20982cf8ddad
Nick DeMarchis <65045396+ndemarchis@users.noreply.github.com>
2024-01-29 18:47:47 +0000

    add Vercel analytics and speed insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Html, Head, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import Script from "next/script";
 
 const MyDocument = () => {
   return (
     <Html lang="en">
       <Head>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-DWVJ6EDJSX"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-DWVJ6EDJSX');
-            `,
-          }}
-        />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </Html>
   );
